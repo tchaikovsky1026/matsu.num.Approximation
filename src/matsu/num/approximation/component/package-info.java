0aefe59727ae5cf833851e1cf467dc6313336719
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-01-12 10:33:39 +0900

    docs: javaファイルのヘッダにCopyright表記の追加(未記載分)

--- src/matsu/num/approximation/component/package-info.java
+++ src/matsu/num/approximation/component/package-info.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2024 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
 /**
  * このモジュールの機能を実現するための横断的基本コンポーネントを扱う非公開パッケージ.
  */
--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2024 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
 /**
  * 関数の近似を扱うモジュール.
  * 
