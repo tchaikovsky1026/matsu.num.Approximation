5c6e58329c7472c077c9c84aeb1ce53631a100ef
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-12-23 16:37:54 +0900

    feat: ApproxTargetにelementTypeProviderを追加し,移行を促す

---
+++ test/matsu/num/approximation/ApproxTargetTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+package matsu.num.approximation;
+
+import org.junit.Test;
+import org.junit.Test.None;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import matsu.num.approximation.PseudoRealNumber.Provider;
+import matsu.num.approximation.PseudoRealNumber.TypeProvider;
+
+/**
+ * {@link ApproxTarget} のテスト
+ */
+@RunWith(Enclosed.class)
+final class ApproxTargetTest {
+
+    public static class 実装における元プロバイダに関する_実装無し {
+
+        private static final class ApproxTargetImpl extends ApproxTarget<DoubleLike> {
+
+            @Override
+            protected DoubleLike calcValue(DoubleLike x) {
+                throw new AssertionError("unreachable");
+            }
+
+            @Override
+            protected DoubleLike calcScale(DoubleLike x) {
+                throw new AssertionError("unreachable");
+            }
+
+            @Override
+            public FiniteClosedInterval<DoubleLike> interval() {
+                throw new AssertionError("unreachable");
+            }
+
+        }
+
+        @Test(expected = AssertionError.class)
+        public void test_実装しない場合アサーションが投げられる_elementProvider() {
+            new ApproxTargetImpl().elementProvider();
+        }
+
+        @Test(expected = AssertionError.class)
+        public void test_実装しない場合アサーションが投げられる_elementTypeProvider() {
+            new ApproxTargetImpl().elementTypeProvider();
+        }
+    }
+
+    public static class 実装における元プロバイダに関する_elementProvider実装 {
+
+        private static final class ApproxTargetImpl extends ApproxTarget<DoubleLike> {
+
+            @Override
+            protected DoubleLike calcValue(DoubleLike x) {
+                throw new AssertionError("unreachable");
+            }
+
+            @Override
+            protected DoubleLike calcScale(DoubleLike x) {
+                throw new AssertionError("unreachable");
+            }
+
+            @Override
+            public FiniteClosedInterval<DoubleLike> interval() {
+                throw new AssertionError("unreachable");
+            }
+
+            @Override
+            public Provider<DoubleLike> elementProvider() {
+                return DoubleLike.elementTypeProvider();
+            }
+        }
+
+        @Test(expected = None.class)
+        public void test_正常に生成_elementProvider() {
+            new ApproxTargetImpl().elementProvider();
+        }
+
+        @Test(expected = None.class)
+        public void test_正常に生成_elementTypeProvider() {
+            new ApproxTargetImpl().elementTypeProvider();
+        }
+    }
+
+    public static class 実装における元プロバイダに関する_elementTypeProvider実装 {
+
+        private static final class ApproxTargetImpl extends ApproxTarget<DoubleLike> {
+
+            @Override
+            protected DoubleLike calcValue(DoubleLike x) {
+                throw new AssertionError("unreachable");
+            }
+
+            @Override
+            protected DoubleLike calcScale(DoubleLike x) {
+                throw new AssertionError("unreachable");
+            }
+
+            @Override
+            public FiniteClosedInterval<DoubleLike> interval() {
+                throw new AssertionError("unreachable");
+            }
+
+            @Override
+            public TypeProvider<DoubleLike> elementTypeProvider() {
+                return DoubleLike.elementTypeProvider();
+            }
+        }
+
+        @Test(expected = None.class)
+        public void test_正常に生成_elementProvider() {
+            new ApproxTargetImpl().elementProvider();
+        }
+
+        @Test(expected = None.class)
+        public void test_正常に生成_elementTypeProvider() {
+            new ApproxTargetImpl().elementTypeProvider();
+        }
+    }
+
+}
