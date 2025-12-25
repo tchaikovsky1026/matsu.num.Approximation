d8593bbeee898c2f2c2ced504d46879d41263a2f
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-12-25 15:32:49 +0900

    docs: update approximation.polynomial.MinimaxDoublePolynomialApproxExecutor, approximation.polynomial.MinimaxPolynomialApproxExecutor

--- src/matsu/num/approximation/polynomial/MinimaxDoublePolynomialApproxExecutor.java
+++ src/matsu/num/approximation/polynomial/MinimaxDoublePolynomialApproxExecutor.java
@@ -5,7 +5,7 @@
  * http://opensource.org/licenses/mit-license.php
  */
 /*
- * 2024.12.26
+ * 2025.12.25
  */
 package matsu.num.approximation.polynomial;
 
@@ -18,6 +18,26 @@ import matsu.num.approximation.component.ApproximationFailedException;
 /**
  * スケーリング付きミニマックス法 (重みづけ Chebyshev ノルム最小化) による,
  * {@code double} 型で表現された実数体に関する多項式関数による近似の実行を扱う.
+ * 
+ * <p>
+ * <i>p</i>(<i>x</i>) を, (最高) 次数が定められた多項式関数空間の元とする. <br>
+ * 近似誤差を, <i>e</i>(<i>x</i>) =
+ * (<i>p</i>(<i>x</i>) - <i>f</i>(<i>x</i>))
+ * /
+ * <i>s</i><sub><i>f</i></sub>(<i>x</i>)
+ * と定め, max<sub><i>x</i></sub> |<i>e</i>(<i>x</i>)|
+ * が最小になるような <i>p</i>(<i>x</i>) を, このクラスの文脈における
+ * <i>f</i>(<i>x</i>)
+ * の近似多項式という.
+ * </p>
+ * 
+ * <p>
+ * このクラスのインスタンスは,
+ * {@link #of(int)} メソッドにより取得する. <br>
+ * 近似多項式の (最高) 次数はこのクラスのインスタンス生成時に確定する. <br>
+ * 多項式近似は, {@link #apply(DoubleApproxTarget)} メソッドにより実行する. <br>
+ * 近似に失敗した場合は "空" が返る.
+ * </p>
  * 
  * <p>
  * 扱うことができる多項式の次数 <i>n</i> は, 次のとおりである. <br>
@@ -41,14 +61,14 @@ public final class MinimaxDoublePolynomialApproxExecutor {
     private final int order;
 
     /**
-     * 与えられた値を近似多項式の次数とする, インスタンスを生成.
+     * 与えられた値を近似多項式の (最高) 次数とする, インスタンスを生成.
      * 
      * @throws IllegalArgumentException 次数が不適の場合
      */
     private MinimaxDoublePolynomialApproxExecutor(int order) {
         if (!(LOWER_LIMIT_OF_ORDER <= order &&
                 order <= UPPER_LIMIT_OF_ORDER)) {
-            throw new IllegalArgumentException("次数が不適");
+            throw new IllegalArgumentException("invalid order: order = " + order);
         }
         this.order = order;
     }
@@ -63,11 +83,11 @@ public final class MinimaxDoublePolynomialApproxExecutor {
     }
 
     /**
-     * <p>
      * 与えられたターゲット関数を近似する.
-     * </p>
      * 
      * <p>
+     * 近似結果の次数 {@link DoublePolynomial#degree()} は,
+     * 自身の {@link #order()} に一致する. <br>
      * 近似の計算中に不具合が出た場合は, 空の {@link ApproxResult} が返る.
      * </p>
      * 
@@ -81,6 +101,9 @@ public final class MinimaxDoublePolynomialApproxExecutor {
                     Objects.requireNonNull(target), this.order);
             //ここで例外が発生する可能性がある.
             calc.calculate();
+
+            assert this.order() == calc.getResult().degree();
+
             return ApproxResult.of(calc.getResult());
         } catch (ApproximationFailedException afe) {
             return ApproxResult.failed(afe.failuerMessage());
@@ -88,9 +111,7 @@ public final class MinimaxDoublePolynomialApproxExecutor {
     }
 
     /**
-     * <p>
      * 与えられた値を近似多項式の次数とする, ミニマックス法による多項式近似エグゼキュータを返す.
-     * </p>
      * 
      * <p>
      * 扱うことができる次数の範囲はこのクラスの定数で規定されている.
